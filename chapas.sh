be95ad6c79b758959fe95ac23ba061c422c3e033
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-02-24 21:42:14 +0900

    initial release

---
+++ chapas.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+DIR=`dirname $0`
+MODULE=${1-'spacy_chapas'}
+LOAD=${2-'load("gendai")'}
+CONLLU=${3-"$DIR/question1.conllu"}
+TMP=/tmp/$MODULE.$$.`basename $CONLLU`
+
+python3 -c '
+import '$MODULE'
+nlp='"$MODULE.$LOAD"'
+with open("'$CONLLU'","r",encoding="utf-8") as f:
+  r=f.read()
+with open("'$TMP'","w",encoding="utf-8") as f:
+  for s in r.split("\n"):
+    if s.startswith("# text = "):
+      doc=nlp(s[9:])
+      for t in doc:
+        print("\t".join([str(t.i+1),t.orth_,t.lemma_,t.pos_,t.tag_,"_",str(0 if t.head==t else t.head.i+1),t.dep_.lower(),"_","_" if t.whitespace_ else "SpaceAfter=No"]),file=f)
+      print("",file=f)
+'
+echo '###' $MODULE.$LOAD $CONLLU
+python3 $DIR/conll18_ud_eval.py $CONLLU $TMP | awk '
+{
+  i=i" / "$1;
+  j=j" / "$NF;
+}
+END{
+  printf("%s = %s\n",substr(i,4),substr(j,4));
+}'
+rm -f $TMP
+exit 0
