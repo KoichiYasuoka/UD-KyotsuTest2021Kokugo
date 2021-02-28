a934bf24029d9d19f5858068d3c710bb227d7b36
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-02-28 09:37:39 +0900

    spaCy v3

--- spacy3.sh
+++ spacy3.sh
@@ -14,9 +14,10 @@ with open("'$TMP'","w",encoding="utf-8") as f:
   for s in r.split("\n"):
     if s.startswith("# text = "):
       doc=nlp(s[9:])
-      for t in doc:
-        print("\t".join([str(t.i+1),t.orth_,t.lemma_,t.pos_,t.tag_,"_",str(0 if t.head==t else t.head.i+1),t.dep_.lower(),"_","_" if t.whitespace_ else "SpaceAfter=No"]),file=f)
-      print("",file=f)
+      for d in doc.sents:
+        for t in d:
+          print("\t".join([str(t.i-d.start+1),t.orth_,t.lemma_,t.pos_,t.tag_,"_",str(0 if t.head==t else t.head.i-d.start+1),t.dep_.lower(),"_","_" if t.whitespace_ else "SpaceAfter=No"]),file=f)
+        print("",file=f)
 '
 echo '###' $MODULE.$LOAD $CONLLU
 python3 $DIR/conll18_ud_eval.py $CONLLU $TMP | awk '
